36890a2d49248c10acbbba4a5d507a4eb54c47f8
officialarshadnoor <arshadnoor585@gmail.com>
2023-10-10 00:04:29 +0530

    Testimonials

---
+++ testimonial/src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Pranay Gupta",
+        job: "Software Engineer",
+        image: "https://images-eu.ssl-images-amazon.com/images/S/influencer-profile-image-prod/logo/countingunique_1661331916943_original._CR290,54,1614,1615_._FMjpg_.jpeg",
+        text: "The power of EddieHub ... when I checked my #GitHub notifications now +9 followers and an email job offer containing the following sentence: I found your profile on GitHub",
+    },
+    {
+        id: 2,
+        name: "Abir Pal",
+        job: "Graphic Designer",
+        image: "https://media.licdn.com/dms/image/D4D03AQEsLbXKqTITXA/profile-displayphoto-shrink_800_800/0/1688111535665?e=2147483647&v=beta&t=V66n3HWRpRVUfDpGtAPPveDQunganAcVfnQ4bkfyQAA",
+        text: "I love EddieHub :heart: my productivity level has increased now i feel more like a developer before i felt like i was forcing it now i enjoy and cherish every moment",
+    },
+    {
+        id: 3,
+        name: "Soumya Banerjee",
+        job: "Marketing Manager",
+        image: "https://res.cloudinary.com/dz209s6jk/image/upload/Avatars/uqflpnwyyani8us9mjqh.jpg",
+        text: "The power of EddieHub ... when I checked my #GitHub notifications now +9 followers and an email job offer containing the following sentence: I found your profile on GitHub",
+    },
+    {
+        id: 4,
+        name: "Saikat Mukherjee",
+        job: "Content Strategist",
+        image: "https://i1.rgstatic.net/ii/profile.image/865279655571458-1583310154826_Q512/Saikat-Mukherjee-8.jpg",
+        text: "y'all should follow @eddiejaoude. he puts so much positive energy into the world. love that guy. he's always trying to support other people and lift them up.",
+    },
+    {
+        id: 5,
+        name: "Mohammad Ahmad",
+        job: "Software Engineer",
+        image: "https://media.licdn.com/dms/image/D4D03AQGO6S8zdEzlOQ/profile-displayphoto-shrink_800_800/0/1670413051348?e=2147483647&v=beta&t=rcg-GeqjjQyyEIM9ddh6DLb58Bg0wFZou87XTWNhPR0",
+        text: "I love EddieHub :heart: my productivity level has increased now i feel more like a developer before i felt like i was forcing it now i enjoy and cherish every moment",
+    },
+];
+
+export default reviews;
